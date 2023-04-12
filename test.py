786e9a732bd5002528679d6bb385a972d0d8922a
JoshCOrange <jchen748@wisc.edu>
2023-04-12 15:06:28 -0400

    Add functionalities in test.py
    Added help() in test.py for checking functions of Btree

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import sqlvalidator
-formatted_sql = sqlvalidator.format_sql("SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders FROM (Orders INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID) GROUP BY LastName HAVING COUNT(Orders.OrderID) > 10 LIMIT 10;")
+import BTrees
+#print(help(BTrees))
+print(help(BTrees.IOBTree))
+'''formatted_sql = sqlvalidator.format_sql("SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders FROM (Orders INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID) GROUP BY LastName HAVING COUNT(Orders.OrderID) > 10 LIMIT 10;")
 
 sql_query = sqlvalidator.parse("SELECT * from table")
 print(sql_query.is_valid())
@@ -9,4 +12,4 @@ sql_query = sqlvalidator.parse(invalid_query)
 print(sql_query.is_valid())
 
 with cte as (select * from a)
-select * from cte
+select * from cte'''
