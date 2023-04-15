bb14679006a12caff5d338023c1603dffe565b96
JoshCOrange <jchen748@wisc.edu>
2023-04-15 18:28:47 -0400

    Complete part of SELECT
    Only did SELECT and  FROM. WHERE clause is only partly done.

--- parser.py
+++ parser.py
@@ -7,15 +7,25 @@ sqlCT = '''CREATE TABLE public.actor (
     last_name character varying(45) NOT NULL,
     last_update timestamp without time zone DEFAULT now() NOT NULL,
     FOREIGN KEY (last_update) REFERENCES Weeks (time) ON DELETE CASCADE,
-    FOREIGN KEY (first_name) REFERENCES Names (first_name) ON DELETE CASCADE,
+    FOREIGN KEY (first_name) REFERENCES Names (first_name) ON DELETE SET NULL,
     Primary Key (actor_id)
     );'''
 sqlCI = "CREATE INDEX ID_test ON t1 (col_1, col_2);"
 sqlDT = "DROP TABLE table_1"
 sqlDI = "DROP INDEX index_1 ON table_1"
-sqlS = "SELECT * FROM table_1"
-qs = [sqlCT,sqlCI,sqlDT, sqlDI]
-#qs = [sqlS]
+sqlS1 = "SELECT col_1 FROM table_1"
+sqlS2 = "SELECT col_1, col_2, col_3, col_4 FROM table_1"
+sqlS3 = "SELECT col_1, col_2, col_3, col_4 FROM table_1, table_2, table_3, table_4, table_5"
+sqlS4 = '''SELECT * FROM PARTS
+            WHERE name LIKE ‘W%’'''
+sqlS5 = '''SELECT *
+            FROM PARTS
+            WHERE name LIKE ‘W%’ OR
+            p# IN (2,4,8) OR
+            p# BETWEEN 11 AND 15'''
+
+#qs = [sqlCT,sqlCI,sqlDT, sqlDI] # Create and Drop table/index
+qs = [sqlS1, sqlS2, sqlS3, sqlS4, sqlS5]
 def readQuery(qs):
     #query = input("Please enter query")
     #sql = "DROP TABLE test"
@@ -38,10 +48,8 @@ def readQuery(qs):
                 if tokens[1].match(sqlparse.tokens.Keyword, 'INDEX'):
                     dropParse(-1, tokens)
             if tokens[0].match(sqlparse.tokens.DML, 'SELECT'):
-                if sql_query.is_valid():
-                    selectParse(tokens)
-                else:
-                    print("Mistake in SELECT statement")
+                selectParse(tokens)
+        print ("---"*20)
 
 
 
@@ -80,18 +88,16 @@ def createParse(flag, tokens): #Design Choice, stop user from using () in naming
                 print (f"column: {c_name}")
                 print (f"date type: {c_type}")
                 #print(type(c[0]))
-            print ("---"*20)
             break
 
         if flag == -1 and token.match(sqlparse.tokens.Keyword, 'ON'):
             print (f"index: {tokens[i-1]}")
-            tableColumns =tokens[i+1].value.split(" ")
-            tableColumns = [''.join(c for c in s if c not in string.punctuation) for s in tableColumns]
-            tableColumns = [s for s in tableColumns if s]
+            tableColumns = tokens[i+1].value.split(" ")
+            tableColumns = [''.join(c for c in s if c not in string.punctuation) for s in tableColumns if s]
+            #tableColumns = [s for s in tableColumns if s]
             print (f"table: {tableColumns[0]}")
             for col in tableColumns[1:]:
                 print (f"column: {col}")
-            print ("---"*20)
             
 def dropParse(flag, tokens):
     if flag == 1:
@@ -103,7 +109,6 @@ def dropParse(flag, tokens):
         if token.match(sqlparse.tokens.Keyword, 'ON'):
             print (f"index: {tokens[i-1]}")
             print (f"table: {tokens[i+1]}")
-            print ("---"*20)
             return 
 
 def updateParse():
@@ -112,7 +117,29 @@ def insertParse():
     pass
 def deleteParse():
     pass
-def selectParse(tokens):
-    print(tokens)
+def selectParse(tokens): #SUM, AVG, MIN, MAX, COUNT, DISTINCT
+    #print(tokens)
+    index_f = -1 #index of 'FROM' in sql statement
+    index_w = -1
+    for i, token in enumerate(tokens):
+        if token.match(sqlparse.tokens.DML, 'SELECT'):
+            columns = tokens[i + 1].value.split(" ")
+            columns = [''.join(c for c in s if c not in "!\"#$%&'()+, -/:;<=>?@[\]^_`{|}~") for s in columns if s]
+            
+        if token.match(sqlparse.tokens.Keyword, 'FROM'): #if at end of select or next token is 'WHERE'
+            tables = tokens[i+1].value.split(" ")
+            tables = [''.join(c for c in s if c not in string.punctuation) for s in tables if s]
+        if token.value.startswith("WHERE"): #only one where and its also the end
+            clause = tokens[i]
+        #print(token)
+
+    #tokens[i].match(sqlparse.tokens.Keyword, 'AND') or tokens[i].match(sqlparse.tokens.Keyword, 'OR')
+    
+    
+    
+    for c in columns:
+        print(f"column: {c}")
+    for t in tables:
+        print(f"table: {t}") 
     return
 readQuery(qs)
