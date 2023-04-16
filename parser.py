86a0f4b47ca9e09895bbf16ec840b28018828383
JoshCOrange <jchen748@wisc.edu>
2023-04-16 19:14:01 -0400

    Parser is done
    Parser is done, outputs a tuple
    (keyword, schemaDict)
    
    Needs some testing on insert, delete, update
    Done by Nitin and Joshua

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import sqlparse
 import string
 import re
+import copy
 
 sqlCT = '''CREATE TABLE public.actor (
     actor_id integer DEFAULT nextval('public.actor_actor_id_seq'::regclass) NOT NULL,
@@ -25,18 +26,27 @@ sqlS5 = '''SELECT *
             p# BETWEEN 11 AND 15 OR
             p# BETWEEN 20 AND 25 OR
             p# IN (2,4,8)'''
-
-
 sqlS6 = '''SELECT col_1, col_2, col_3, col_4 FROM table_1
             WHERE col_1 < 10
-            ORDER BY col_2'''
+            ORDER BY col_2, col_1 DESC, col_3 ASC, col_4'''
 
-#qs = [sqlCT,sqlCI,sqlDT, sqlDI] # Create and Drop table/index
-#qs = [sqlS1, sqlS2, sqlS3, sqlS4, sqlS5]
-qs = [sqlS6]
+sqlS7 = '''SELECT col_1, col_2, col_3, col_4 FROM table_1
+            WHERE col_1 < 10
+            GROUP BY col_2, col_4
+            ORDER BY col_1 DESC, col_3 ASC'''
+
+sqlS8 = '''SELECT COUNT(CustomerID), Country
+            FROM Customers
+            GROUP BY Country
+            HAVING COUNT(CustomerID) BETWEEN 0 and 5;'''
+
+qs = [sqlCT,sqlCI,sqlDT, sqlDI] # Create and Drop table/index
+#qs = [sqlS1, sqlS2, sqlS3, sqlS4, sqlS5, sqlS6,sqlS7, sqlS8]
+#qs = [sqlS8]
 def readQuery(qs):
     #query = input("Please enter query")
     #sql = "DROP TABLE test"
+    schemaDict = {}
     for sql in qs:
         tiflag = 0 # 1 for table, -1 for index, 0 for other
         parsed = sqlparse.parse(sql)
@@ -47,16 +57,35 @@ def readQuery(qs):
             #temp = str(tokens[0])
             if tokens[0].match(sqlparse.tokens.DDL, 'CREATE'):
                 if tokens[1].match(sqlparse.tokens.Keyword, 'TABLE'):
-                    createParse(1,tokens)
+                    schemaDict = createParse(1,tokens)
+                    execution = ('create table', schemaDict)
                 if tokens[1].match(sqlparse.tokens.Keyword, 'INDEX'):
-                    createParse(-1, tokens)
+                    schemaDict = createParse(-1, tokens)
+                    execution = ('create index', schemaDict)
             if tokens[0].match(sqlparse.tokens.DDL, 'DROP'): #Should we handle if exists?
                 if tokens[1].match(sqlparse.tokens.Keyword, 'TABLE'):
-                    dropParse(1,tokens)
+                    schemaDict = dropParse(1,tokens)
+                    execution = ('drop table', schemaDict)
                 if tokens[1].match(sqlparse.tokens.Keyword, 'INDEX'):
-                    dropParse(-1, tokens)
+                    schemaDict = dropParse(-1, tokens)
+                    execution = ('drop index', schemaDict)
             if tokens[0].match(sqlparse.tokens.DML, 'SELECT'):
-                selectParse(tokens, stmt)
+                schemaDict = selectParse(tokens, stmt)
+                execution = ('select', schemaDict)
+            if tokens[0].match(sqlparse.tokens.DML, 'UPDATE'):
+                schemaDict = updateParse(tokens)
+                execution = ('update', schemaDict)
+            if tokens[0].match(sqlparse.tokens.DML, 'INSERT'):
+                schemaDict = insertParse(tokens)
+                execution = ('insert', schemaDict) 
+            if tokens[0].match(sqlparse.tokens.DML, 'DELETE'):
+                flag = 1
+                for i, token in enumerate(tokens):
+                    if token.value.startswith("WHERE"): flag = 0
+                schemaDict = deleteParse(tokens)
+                execution = ('deleteParse', schemaDict)
+            
+        print(schemaDict)
         print ("---"*20)
 
 
@@ -103,17 +132,21 @@ def whereParse(clause):
         whereSchemaDict = {"conditions": conditions, "conjunctions": conjunctions}
         return whereSchemaDict
         
-        
-        '''
-        print(condis)
-        print(conjunc)'''
 
 def createParse(flag, tokens): #Design Choice, stop user from using () in naming anything
+    schemaDict = {}
     for i, token in enumerate(tokens):
         if flag == 1 and token.value.startswith("("):
+            column_names = []
+            column_types = []
+            foreign_keys = []
+            foreign_tables = []
+            foreign_columns = []
+            foreign_deletes = []
             # Get the table name by looking at the tokens in reverse order till you find
             # a token with None type
-            print (f"table: {get_table_name(tokens[:i])}")
+            #print (f"table: {get_table_name(tokens[:i])}")
+            schemaDict.update({"table_name": get_table_name(tokens[:i])})
 
             # Now parse the columns
             txt = token.value
@@ -125,42 +158,65 @@ def createParse(flag, tokens): #Design Choice, stop user from using () in naming
                 # OR 
                 #c_type = " ".join(c[1:]) # For detailed type information 
                 if c[0].casefold() == "Primary".casefold():
-                    print(f"Primary key: {c[2][1:-1]}")
+                    #print(f"Primary key: {c[2][1:-1]}")
+                    schemaDict.update({"primary_key": c[2][1:-1]})
                     continue
                 if c[0].casefold() == "Foreign".casefold():
-                    print(f"Foreign key: {c[2][1:-1]}")
-                    print(f"    Referenced Table: {c[4][0:-1]}")
-                    print(f"    Referenced Row: {c[5][1:-1]}")
+                    #print(f"Foreign key: {c[2][1:-1]}")
+                    foreign_keys.append(c[2][1:-1])
+                    #print(f"    Referenced Table: {c[4][0:-1]}")
+                    foreign_tables.append(c[4][0:-1])
+                    #print(f"    Referenced Column: {c[5][1:-1]}")
+                    foreign_columns.append(c[5][1:-1])
                     delete = " ".join(c[8:])
-                    print(f"    Delete Type: {delete}")
+                    #print(f"    Delete Type: {delete}")
+                    foreign_deletes.append(delete)
+                    schemaDict.update({"foreign_keys": foreign_keys})
+                    schemaDict.update({"foreign_tables": foreign_tables})
+                    schemaDict.update({"foreign_columns": foreign_columns})
+                    schemaDict.update({"foreign_deletes": foreign_deletes})
                     continue
-                print (f"column: {c_name}")
-                print (f"date type: {c_type}")
+                #print (f"column: {c_name}")
+                column_names.append(c_name)
+                #print (f"date type: {c_type}")
+                column_types.append(c_type)
                 #print(type(c[0]))
+                schemaDict.update({"column_names": column_names})
+                schemaDict.update({"column_types": column_types})
             break
 
         if flag == -1 and token.match(sqlparse.tokens.Keyword, 'ON'):
-            print (f"index: {tokens[i-1]}")
+            #print (f"index: {tokens[i-1]}")
+            column_names = []
+            schemaDict.update({"index_name": tokens[i-1].value})
             tableColumns = tokens[i+1].value.split(" ")
             tableColumns = [''.join(c for c in s if c not in string.punctuation) for s in tableColumns if s]
             #tableColumns = [s for s in tableColumns if s]
-            print (f"table: {tableColumns[0]}")
+            #print (f"table: {tableColumns[0]}")
+            schemaDict.update({"table_name": tableColumns[0]})
             for col in tableColumns[1:]:
-                print (f"column: {col}")
+                #print (f"column: {col}")
+                column_names.append(col)
+            schemaDict.update({"column_names": column_names})
+    #print(schemaDict)
+    return schemaDict
             
 def dropParse(flag, tokens):
+    schemaDict = {}
     if flag == 1:
         #print("Drop Table")
-        print (f"table: {tokens[-1]}")
-        print ("---"*20)
-        return
+        #print (f"table: {tokens[-1]}")
+        schemaDict.update({"table_name": tokens[-1].value})
+        return schemaDict
     for i, token in enumerate(tokens):
         if token.match(sqlparse.tokens.Keyword, 'ON'):
-            print (f"index: {tokens[i-1]}")
-            print (f"table: {tokens[i+1]}")
-            return 
+            #print (f"index: {tokens[i-1]}")
+            schemaDict.update({"index_name": tokens[i-1].value})
+            #print (f"table: {tokens[i+1]}")
+            schemaDict.update({"table_name": tokens[i+1].value})
+    return schemaDict
 
-def updateParse(flag, tokens): #assumes values are encolsed in single quotes
+def updateParse(tokens): #assumes values are encolsed in single quotes
     tableName = tokens[2].value
     setClause = None
     for token in tokens:
@@ -188,7 +244,7 @@ def updateParse(flag, tokens): #assumes values are encolsed in single quotes
         schemaDict = {'table': tableName, 'columns': columns, 'values': values}
         return schemaDict #or whatever to send to execution
 
-def insertParse(flag, tokens): #INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);
+def insertParse(tokens): #INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);
     tableName = None
     columns = []
     values = []
@@ -243,35 +299,54 @@ def deleteParse(flag, tokens): #assumes flag is 1 if no WHERE clause exists
 
 def selectParse(tokens, stmt): #SUM, AVG, MIN, MAX, COUNT, DISTINCT
     #print(tokens)
-    index_f = -1 #index of 'FROM' in sql statement
-    index_w = -1
-    clause = ""
     schemaDict = {}
+
     for i, token in enumerate(tokens):
         if token.match(sqlparse.tokens.DML, 'SELECT'):
             columns = tokens[i + 1].value.split(" ")
             columns = [''.join(c for c in s if c not in "!\"#$%&'()+, -/:;<=>?@[\]^_`{|}~") for s in columns if s]
             #String.punctuation is not used because we want to preserve * in select statment
-            
+            schemaDict.update({"columns": columns})
+        
         if token.match(sqlparse.tokens.Keyword, 'FROM'): #if at end of select or next token is 'WHERE'
-            tables = tokens[i+1].value.split(" ")
-            tables = [''.join(c for c in s if c not in string.punctuation) for s in tables if s]
-        if token.value.startswith("WHERE"): #only one where and its also the end
+            table_names = tokens[i+1].value.split(" ")
+            tables_names = [''.join(c for c in s if c not in string.punctuation) for s in table_names if s]
+            schemaDict.update({"table": tables_names}) 
+        
+        if token.value.startswith("WHERE"):
             clause = str(tokens[i])
-            #print()
+            parsedWhere = whereParse(clause)
+            schemaDict.update({"where": parsedWhere})
+        
+        if token.match(sqlparse.tokens.Keyword, 'GROUP BY'):
+            group_by = []
+            group_stmt = tokens[i + 1].value.split(",")
+            for col in group_stmt:
+                group_by.append(col.strip())
+            schemaDict.update({"group_by": group_by})
+        
+        if token.match(sqlparse.tokens.Keyword, 'HAVING'):
+            clause = str(tokens[i+1])
+            parsedHaving = whereParse(clause)
+            schemaDict.update({"having":parsedHaving}) 
+        
         if token.match(sqlparse.tokens.Keyword, 'ORDER BY'):
-            columns = tokens[i + 1].value.split(" ")
-    
-        #print(token)
+            order_stmt = tokens[i + 1].value.split(",")
+            col_orders = []
+            orders = []
+            for col in order_stmt:
+                tmp = col.strip().split(" ")
 
-    #tokens[i].match(sqlparse.tokens.Keyword, 'AND') or tokens[i].match(sqlparse.tokens.Keyword, 'OR')
-    schemaDict.update({"columns": columns})
-    schemaDict.update({"table": tables}) 
-    if clause:
-        parsedWhere = whereParse(clause)
-        schemaDict.update(parsedWhere) 
-    print(schemaDict)
-    return
+                col_orders.append(tmp[0])
+                if len(tmp) > 1:
+                    orders.append(tmp[1].upper())
+                else:
+                    orders.append("ASC")
+            schemaDict.update({"oreder_by": {"col_orders": col_orders,"orders": orders }})
+
+
+    #print(schemaDict)
+    return schemaDict
 
 
 
