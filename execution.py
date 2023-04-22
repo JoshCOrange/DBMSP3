079396920c2170d74a8d2107c6879927e1c7dcb2
JoshCOrange <jchen748@wisc.edu>
2023-04-22 17:33:13 -0400

    Create execution.py

---
+++ execution.py
@@ -0,0 +1,7 @@
+#Recieve schema dictionary and keyword pair
+#Calls the correct function in treee.py
+#Keep track of index trees of each taable
+#Group By needs to be handled and Having
+#Order By is not handled
+#There is no interface yet
+#
